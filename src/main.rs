5fb26e1faf81f78f5097bc010b8f18fce9af9f20
mio256 <reiya@woorth.io>
2022-11-10 19:07:02 +0900

    :sparkles: rand

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
+use rand::Rng;
+use std::io;
+
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    println!("The secret number is: {}", secret_number); //秘密の数字は次の通り: {}
 
     println!("Please input your guess.");
 
     let mut guess = String::new();
 
-    let result = std::io::stdin().read_line(&mut guess);
-    result.expect("Failed to read line");
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
 
     println!("You guessed: {}", guess);
 }
