7af14781b2954cd475fc171aff0831cc99869152
mio256 <reiya@woorth.io>
2022-11-11 17:06:53 +0900

    :fire: parse() Ok,Err

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("The secret number is: {}", secret_number);
+    // println!("The secret number is: {}", secret_number);
 
     loop {
         println!("Please input your guess.");
@@ -17,7 +17,15 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse().expect("Please type a number");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please input number");
+                continue;
+            }
+        };
+
+        println!("You guessed: {}", guess);
 
         match guess.cmp(&secret_number) {
             Ordering::Equal => {
@@ -27,7 +35,5 @@ fn main() {
             Ordering::Less => println!("Too small."),
             Ordering::Greater => println!("Too big."),
         }
-
-        println!("You guessed: {}", guess);
     }
 }
